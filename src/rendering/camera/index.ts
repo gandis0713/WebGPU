03fa8c0db77ed00053a1c0205213040f3b19d4ec
gandis <gandis0713@gmail.com>
2021-05-05 12:23:17 +0900

    refactor: change orthographci camera name

--- src/rendering/camera/index.ts
+++ src/rendering/camera/index.ts
@@ -1,5 +1,5 @@
 import AbstractCamera from './abstract_camera';
-import OrthoGraphicCamera from './ortho_graphic_camera';
+import OrthoGraphicCamera from './orthographic_camera';
 import PerspectiveCamera from './perspective_camera';
 
 export default { AbstractCamera, OrthoGraphicCamera, PerspectiveCamera };
